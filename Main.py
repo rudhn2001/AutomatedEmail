c803a53c29fd1fdfb683063ea60ae44f8c7c3b2f
rudhn2001 <nayakanirudh43@gmail.com>
2022-05-17 00:07:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import  re
 
 
 #Email address Validation
-pattern= '^[a-z 0-9]+[\._}?[a-z 0-9]+[@]\w+[.]\w{2,3}+[.]\w{2}$'
+pattern= '([a-z 0-9_.+-]+@[a-z 0-9]+\.[a-z 0-9 -.]+)'
 user_id=input('Enter the email id : ')
-if re.search(pattern,user_id):
+if re.search(pattern, user_id):
     print("The email is valid....")
     
 else: 
